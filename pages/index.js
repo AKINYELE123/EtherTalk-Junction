e84f84cd99333326c1d0c543b7a919021933a74a
AKINYELE123 <boluwatifeakinyele1@gmail.com>
2023-12-06 15:03:42 +0100

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
 import React, { useContext } from 'react'
 
 // import { EtherTalkContext } from '../context/EthertalkContext'
-import { Filter, Friend } from '../components/index';
+import {  Friend } from '../components/index';
 
 const EtherTalk = () => {
 // const {} = useContext(EtherTalkContext);
   return (
     <div>
-      <Filter />
       <Friend />
     </div>
   )
